d56b1024a883c6fbaa132195c450f6fdaab9b959
RhythmSancheti <78738545+RhythmSancheti@users.noreply.github.com>
2021-12-21 15:56:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+Webcam.set({
+    Width: 350,
+    Height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version: ', ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/FpMGkth7a/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The First Prediction is "+prediction_1;
+    speak_data_2="The Second Prediction is"+prediction_2;
+    var utterThis=newSpeechSynthesisUtterance(speak_data1+speak_data2);
+    synth.speak(utterThis);
+}
